134989387f562187bb61e5f63ebb7f73f4a9df02
Baran Uygur <52936700+Trustedev@users.noreply.github.com>
2023-10-21 13:16:02 +0300

    added main files
    if you are looking for "secim.txt"
    directory : \bin\Debug\net6.0\

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ElectionSystem
+{    
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            VoteScanner scan = new VoteScanner();
+            scan.scanVotes("secim.txt");            
+            scan.printVoteTable();    
+
+           
+
+        }
+    }
+}
